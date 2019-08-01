30a751905b6d3ecdc30ae42dfd853659b53a76bb
srikanth <sreekanthmattupalli@gmail.com>
2019-08-01 11:09:17 +0530

    srikanth

--- Basic.py
+++ Basic.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-A=[10,11,12,13,14,15]
-B=[34,52,47,22,45,28]
-plt.xlabel("year")
-plt.ylabel("Temperature")
-plt.title("weather")
-plt.plot(A,B ,color='green' ,linewidth=5,linestyle='dotted' )
-plt.show()
+# A=[10,11,12,13,14,15]
+# B=[34,52,47,22,45,28]
+# plt.xlabel("year")
+# plt.ylabel("Temperature")
+# plt.title("weather")
+# plt.plot(A,B ,color='green' ,linewidth=5,linestyle='dotted' )
+# plt.show()
+
+c=20
+d=30
+print(c+d)
 
 
 # i am added new text
